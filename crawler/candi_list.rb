3504d6fbe1da9f774a8ced220d7a23361efd2fef
jaypark <jaypark@gmail.com>
2016-03-24 16:53:41 +0900

    정식후보 수집기로 변신

--- crawler/candi_list.rb
+++ crawler/candi_list.rb
@@ -27,6 +27,14 @@ CANDI_LIST= '/electioninfo/electionInfo_report.xhtml'+
             'statementId=PCRI03_%232'+
             '&electionCode=2&cityCode=0&sggCityCode=0&townCode=-1&sggTownCode=0&x=29&y=15'
 
+CANDI_LIST= "/electioninfo/electionInfo_report.xhtml"+
+            "?electionId=0020160413&"+
+            "requestURI=%2FWEB-INF%2Fjsp%2Felectioninfo%2F0020160413%2Fcp%2Fcpri03.jsp&"+
+            "topMenuId=CP&secondMenuId=CPRI03&menuId=CPRI03&"+
+            "statementId=CPRI03_%232&"+
+            "electionCode=2&sggCityCode=0&cityCode="
+
+
 # 후보 통계 페이지
 CANDI_SUM = '/electioninfo/electionInfo_report.xhtml'+
             '?electionId=0020160413'+
@@ -34,89 +42,73 @@ CANDI_SUM = '/electioninfo/electionInfo_report.xhtml'+
             '&topMenuId=PC&secondMenuId=PCRI01&menuId='+
             '&statementId=PCRI01_%232&electionCode=2&cityCode=0&x=37&y=12'
 
-
-
-
-STDERR.puts "retreiving : http://#{NEC_SERVER}#{CANDI_LIST}"
-#http = Net::HTTP.new(NEC_SERVER, 80)
-#http.read_timeout = 500
-list_html   = Net::HTTP.get(NEC_SERVER, CANDI_LIST)
-list_doc    = Nokogiri::HTML(list_html)
-
-# 2016 선거구 목록 가져오기
-city_list   = {}
-city_html   = list_doc.css("select#cityCode")
-city_html.css("option").each do |city|
-  city_list[city.attribute_nodes[0].to_s] = city.content
-end
+#cities = [1100, 2600, 2700, 2800, 2900, 3000, 3100, 5100, 4100, 4200, 4300, 4400, 4500, 4600, 4700, 4800, 4900]
+cities = {'1100'=> "서울특별시", '2600'=> "부산광역시", '2700'=> "대구광역시", 
+          '2800'=> "인천광역시", '2900'=> "광주광역시", '3000'=> "대전광역시", 
+          '3100'=> "울산광역시", '5100'=> "세종특별자치시", '4100'=> "경기도", 
+          '4200'=> "강원도",     '4300'=> "충청북도", '4400'=> "충청남도", '4500'=> "전라북도", 
+          '4600'=> "전라남도",   '4700'=> "경상북도", '4800'=> "경상남도", '4900'=> "제주특별자치도"}
 
 candi_list  = []
 
-list_doc.css("table#table01").css("tr").each_with_index do |tr, idx|
+cities.each do |city, cityname|
 
-  candi_array =  tr.css("td")
-  h = {}
-  if candi_array.size>0
-    #STDERR.puts "==="
-    #STDERR.puts candi_array.at(3).to_s
-    #STDERR.puts candi_array.at(2).children[1].to_s
-    #STDERR.puts "==="
-    #exit
-    photo = candi_array.at(2).children[1].attribute_nodes[1].to_s if candi_array.at(2).children[1]
+  STDERR.puts "retreiving : http://#{NEC_SERVER}#{CANDI_LIST}"+city.to_s
+  #http = Net::HTTP.new(NEC_SERVER, 80)
+  #http.read_timeout = 500
+  list_html   = Net::HTTP.get(NEC_SERVER, CANDI_LIST+city.to_s)
+  list_doc    = Nokogiri::HTML(list_html)
 
-    # 맨 뒤의 파일이름이 후보의 유니크 아이디인 듯.
-    # photo = http://info.nec.go.kr/photo_20160413/Sd1100/Gsg1101/Sgg2110101/Hb100118435/gicho/100118435.jpg
+  # 2016 선거구 목록 가져오기
 
-    #h[:id]              = photo.scan( /\/gicho\/(.+)\./).first[0].to_s
-    #STDERR.puts candi_array.at(2).to_s
-    #STDERR.puts candi_array.at(3).to_s
-    h[:id]              = candi_array.at(3).to_s.scan( /popupPreHBJ\((.+)/)[0].to_s.split("'")[3]
-    #STDERR.puts h[:id]
-    
-    # Sd 가 시도를 의미하고, 뒤의 숫자와 select#cityCode 의 숫자를 이용하면 시도이름을 얻을 수 있다.
+  list_doc.css("table#table01").css("tr").each_with_index do |tr, idx|
 
-    h[:city]            = city_list[photo.scan( /\/Sd(.+)\/Gsg/).first[0].to_s] if photo
+    candi_array =  tr.css("td")
+    h = {}
+    if candi_array.size>0
+      next if candi_array.to_s.include?('검색된 결과가 없습니다.')
+      #STDERR.puts candi_array
+      #exit
+      photo = candi_array.at(2).children[1].attribute_nodes[1].to_s if candi_array.at(2).children[1]
+
+      # 맨 뒤의 파일이름이 후보의 유니크 아이디인 듯.
+      # photo = http://info.nec.go.kr/photo_20160413/Sd1100/Gsg1101/Sgg2110101/Hb100118435/gicho/100118435.jpg
+
+      #h[:id]              = photo.scan( /\/gicho\/(.+)\./).first[0].to_s
+      #STDERR.puts candi_array.at(2).to_s
+      #STDERR.puts candi_array.at(3).to_s
+      h[:id]              = candi_array.at(3).to_s.scan( /popupPreHBJ\((.+)/)[0].to_s.split("'")[3]
+      #STDERR.puts h[:id]
+      
+      # Sd 가 시도를 의미하고, 뒤의 숫자와 select#cityCode 의 숫자를 이용하면 시도이름을 얻을 수 있다.
+
+      h[:city]            = cityname
 
 
-    h[:district]        = candi_array.at(0).content
-    h[:district_long]   = "#{h[:city]}/#{h[:district]}"
-    h[:party]           = candi_array.at(1).content
-    h[:picture]         = PIC_PREFIX+candi_array.at(2).children[1].attribute_nodes[1] if photo
-    h[:name]            = candi_array.at(3).content.strip 
-    h[:gender]          = candi_array.at(4).content.strip
-    h[:age]             = candi_array.at(5).content      
-    h[:address]         = candi_array.at(6).content      
-    h[:occupation]      = candi_array.at(7).content   
-    h[:education]       = candi_array.at(8).content  
-    h[:career]          = candi_array.at(9).content
-    h[:criminal_record] = candi_array.at(10).content  
-    #h[:raw]            = candi_array
+      h[:district]        = candi_array.at(0).content
+      h[:district_long]   = "#{h[:city]}/#{h[:district]}"
+      h[:picture]         = PIC_PREFIX+candi_array.at(1).children[1].attribute_nodes[1] if photo
+      h[:party]           = candi_array.at(3).content
+      h[:name]            = candi_array.at(4).content.strip 
+      h[:gender]          = candi_array.at(5).content.strip
+      h[:age]             = candi_array.at(6).content      
+      h[:address]         = candi_array.at(7).content      
+      h[:occupation]      = candi_array.at(8).content   
+      h[:education]       = candi_array.at(9).content  
+      h[:career]          = candi_array.at(10).content
+      h[:asset]           = candi_array.at(11).content.gsub(',','')
+      h[:military]        = candi_array.at(12).content  
+      h[:tax]             = candi_array.at(13).content.gsub(',','')
+      h[:tax_not_thisyear]= candi_array.at(14).content.gsub(',','')
+      h[:tax_not]         = candi_array.at(15).content.gsub(',','')
+      h[:criminal_record] = candi_array.at(16).content  
+
+      #h[:raw]            = candi_array
+    end
+    candi_list << h unless h.empty?
   end
-  candi_list << h unless h.empty?
-
 end
 
 
 # 출력!
 puts JSON.pretty_generate(candi_list)
-
-
-# 통계페이지에서 전체 후보 숫자 가져와서 검증
-
-sum_html = Net::HTTP.get(NEC_SERVER, CANDI_SUM)
-
-Nokogiri::HTML(sum_html).css("table#table01").css("tr").each_with_index do |tr, idx|
-  if idx==2
-    sum_array =  tr.css("td").children.to_a
-    if sum_array[3].content.to_i != candi_list.size
-      STDERR.puts "ERROR: candidates : #{sum_array[3].content}, crawled: #{candi_list.size}"
-
-      # 오류 발생!!
-      exit
-    else
-      STDERR.puts "candidates : #{sum_array[3].content}, crawled: #{candi_list.size}"
-    end
-  end
-end
-
-
